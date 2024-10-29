e20ea3e631d38dab8eb15d64c0e74f22c15601d7
intikhab-h-bhat <intikhabh@gmail.com>
2024-10-29 08:11:34 +0530

    select particular columns from table

--- Basic/select-query.py
+++ Basic/select-query.py
@@ -1,3 +1,8 @@
 
-# This will select all rows from celebs table
-Select * from celebes
+# This will select all rows from city table
+Select * from world.city
+
+# select all names 
+select Name from world.city 
+
+select ID,Name,CountryCode from world.city 
