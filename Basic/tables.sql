bdbfd6b8730380b196c678c47f4841d4f316b492
intikhab-h-bhat <intikhabh@gmail.com>
2024-10-30 21:24:03 +0530

    add ,update column and drop columns from existing table

--- Basic/tables.sql
+++ Basic/tables.sql
@@ -29,11 +29,13 @@ values (1, 'Shubham', 'Thakur', 'India','23',1234567890),
 --  select * from customer
 
 
---  create table from existing table
+--  create table from existing table with data
 
 create table sub_cust  as 
 select CustomerName,LastName from customer;
 
+--  create table from existing table with out data by using the where clause with false condition
+create table sub_cust as select * from customer where 1!=1;
 
 -- to delete the table 
 drop table sub_cust
@@ -41,3 +43,19 @@ drop table sub_cust
 --  to delete the data in table
 delete from gfg_employees
 where name='Rithvik';
+
+-- to rename a table
+alter table gfg_employees rename to test_emp
+
+-- to rename a column
+alter table gfg_employees rename column name  to first_name
+
+--  to add a column in existing table
+alter table temp2 add column salary decimal(10,2);
+
+--  to drop a column from table
+alter table tem2 drop column phpne
+
+--  add two columns in existing table
+ALTER TABLE temp2 ADD 
+ (departid number(3),depart varchar(40));
