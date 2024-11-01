747164e65dfdfb48f29258775af39a424d04b282
intikhab-h-bhat <intikhabh@gmail.com>
2024-11-01 20:45:07 +0530

    group by

---
+++ Basic/groupby-having.sql
@@ -0,0 +1,9 @@
+
+-- For each continent show the continent and number of countries.
+select continent, count(name) from world
+group by continent
+
+
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+select continent, count(name) from world where population >=10000000
+group by continent
